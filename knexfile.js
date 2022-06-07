00cbd73e09e6cfc05798c685e12d3c6fb4a9f027
Êxodo <exodowellis@gmail.com>
2022-06-06 21:02:14 -0300

    created table movies_notes

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const { PassThrough } = require('stream');
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     },
     useNullAsDefault: true
   }
